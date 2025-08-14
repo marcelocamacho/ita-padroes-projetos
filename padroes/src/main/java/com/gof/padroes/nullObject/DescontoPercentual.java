6e4f8283309d1ff7a66961ac2499d0098c512db7
Marcelo Santana Camacho <marcelocamacho.ufpa@gmail.com>
2025-08-13 23:38:09 -0300

    Exemplo de código que faz o mal uso do null

---
+++ padroes/src/main/java/com/gof/padroes/nullObject/DescontoPercentual.java
@@ -0,0 +1,16 @@
+package com.gof.padroes.nullObject;
+
+public class DescontoPercentual implements Desconto{
+
+    private double percentual;
+
+    public DescontoPercentual(double percentual){
+        super();
+        this.percentual = percentual;
+    }
+    @Override
+    public double darDesconto(double valorOriginal) {
+        return (100.0 - percentual) / 100 * valorOriginal;
+    }
+
+}
--- padroes/src/test/java/com/gof/padroes/TesteItem.java
+++ padroes/src/test/java/com/gof/padroes/TesteItem.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
+import com.gof.padroes.nullObject.DescontoAbsoluto;
+import com.gof.padroes.nullObject.DescontoPercentual;
 import com.gof.padroes.nullObject.Item;
 
 public class TesteItem {
@@ -13,5 +15,20 @@ public class TesteItem {
         Item i = new Item("tenis", 150.0);
         assertEquals(300.00, i.precoQuantidade(2), 0.001);
         assertEquals("tenis R$150.0", i.toString());
+    }
+
+    @Test
+    public void testDescontoPercentual(){
+        Item i = new Item("tenis", 150.00, new DescontoPercentual(10));
+        assertEquals(270.0, i.precoQuantidade(2), 0.001);
+        assertEquals("tenis R$135.0", i.toString());
+    }
+
+    @Test
+    public void testDescontoAbsoluto(){
+        Item i = new Item("tenis", 150.00, new DescontoAbsoluto(10));
+        assertEquals(280.0,i.precoQuantidade(2),0.001);
+        assertEquals("tenis R$140.0", i.toString());
+
     }
 }
